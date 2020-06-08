f7110897960765b39bac9f817195be163e239df4
unknown <lecastano6@poligran.edu.co>
2020-06-08 14:44:50 -0500

    se agrega a index.js el require de hogar

--- src/routes/hogar.routes.js
+++ src/routes/hogar.routes.js
@@ -3,4 +3,5 @@ const router = express.Router()
 const hogarController= require('../controllers/hogar.controller')
 //creacion de rutas
 router.get('/productosHogar',hogarController.get);
+
 module.exports = router
